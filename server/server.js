266d41118c9b34db0c1f650f645275247a06e4f6
ahmedosamaalam <h.ahmedosama@gmail.com>
2018-02-07 12:29:17 +0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -10,18 +10,35 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,"../client");
 app.use(express.static(publicPath));
 
+//connection special
 io.on("connection", (socket)=>{
   console.log('New User connected');
 
-  socket.on('disconnect',()=>{
+//message send server to client
+  // io.emit('newMessage', {
+  //   success: true,
+  //   message: {
+  //       name : 'osama',
+  //       email:'asd@gmail.com'}
+  // });
 
+  socket.on('createMessage',function (userMessageData) {
+    console.log('Message from  user',userMessageData);
+    io.emit('newMessage',{
+      success: userMessageData.success,
+      message:userMessageData.message
+    });
+  });
+
+//disconnection special
+  socket.on('disconnect',function(){
     console.log('Disconneted user');
   });
 
 
 });
 
-server.listen(port, ()=> {
+server.listen(port, function() {
 
   console.log("Server is running on port "+port);
 });
